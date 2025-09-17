543bfb5bb9d821b48ba74d0bb61989f29a08b209
000FLMS <2477219512@qq.com>
2025-09-17 15:55:46 -0400

    Feat: RoPE

--- rope.py
+++ rope.py
@@ -50,8 +50,8 @@ def apply_rotary_emb(
 
     _, seqlen, _, _ = query.shape
     device = query.device
-    # todo
-    #
+   
+   
     # Please refer to Lecture 5 slides in https://cmu-l3.github.io/anlp-fall2025/static_files/anlp-f2025-05-transformers.pdf
     # and Section 3 in https://arxiv.org/abs/2104.09864.
 
@@ -60,16 +60,32 @@ def apply_rotary_emb(
     key_real, key_imag = key.float().reshape(key.shape[:-1] + (-1, 2)).unbind(-1)
     # This separates each query/key vector into its odd and even indices (assuming *one-indexing*).
     # query_real contains q_1, q_3, q_5, ... and query_imag contains q_2, q_4, q_6, ...
+    query_real = query_real.transpose(1,2)
+    query_imag = query_imag.transpose(1,2)
+    key_real = key_real.transpose(1,2)
+    key_imag = key_imag.transpose(1,2)
 
     # First, compute the trigonometric values in the second and fourth columns in
     # slide 49 (linked above).
+    
+    dim = head_dim // 2
+    position = torch.arange(0, seqlen, device=device).reshape((seqlen, 1)).broadcast_to((seqlen, dim))
+    theta_rotate = torch.arange(0, dim, device=device).broadcast_to((seqlen, dim))
+    theta_rotate = theta ** (-2.0 * theta_rotate / head_dim)
+    theta_rotate = position * theta_rotate
+    sin_rotate = torch.sin(theta_rotate)
+    cos_rotate = torch.cos(theta_rotate)
+
 
     # Then, combine these trigonometric values with the tensors query_real, query_imag,
     # key_real, and key_imag.
+    rotate_qleft = torch.stack((query_real * cos_rotate, query_imag * cos_rotate), dim=-1).flatten(-2)
+    rotate_qright = torch.stack((-query_imag * sin_rotate, query_real * sin_rotate), dim=-1).flatten(-2)
 
-    raise NotImplementedError
+    rotate_kleft = torch.stack((key_real * cos_rotate, key_imag * cos_rotate), dim=-1).flatten(-2)
+    rotate_kright = torch.stack((-key_imag * sin_rotate, key_real * sin_rotate), dim=-1).flatten(-2)
 
-    query_out = None
-    key_out = None
+    query_out = (rotate_qleft + rotate_qright).transpose(1,2)
+    key_out = (rotate_kleft + rotate_kright).transpose(1,2)
     # Return the rotary position embeddings for the query and key tensors
     return query_out, key_out
